0cfe488660730e11ad3bf1f610bec1298fdce08f
Antfeeder <jesuskevinsilva@gmail.com>
2020-03-12 14:39:16 -0700

    Scripts Added and Adapted

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameControl : MonoBehaviour
+{
+    public static event Action HandlePulled = delegate { };
+
+    [SerializeField]
+    private Text prizeText;
+
+
+    [SerializeField]
+    private Row[] rows;
+
+    [SerializeField]
+    private Transform handle;
+
+    private int prizeValue;
+    
+
+    private bool resultsChecked = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!rows[0].rowStopped || !rows[1].rowStopped || !rows[2].rowStopped || !rows[3].rowStopped){
+            prizeValue = 0;
+            prizeText.enabled = false;
+            resultsChecked = false;
+
+        }
+        if (rows[0].rowStopped && rows[1].rowStopped &&  rows[2].rowStopped &&  rows[3].rowStopped && !resultsChecked){
+            CheckResults();
+            prizeText.enabled = true;
+            prizeText.text = "Prize: " + prizeValue;
+            StartCoroutine("PullHandle");
+        }
+     
+    }
+    private void OnMouseDown(){
+
+        if (rows[0].rowStopped && rows[1].rowStopped && rows[2].rowStopped && rows[3].rowStopped){
+            StartCoroutine("PullHandle");
+            
+        }
+    }
+
+    private IEnumerator PullHandle(){
+        for (int i = 0; i < 15;i+=5){
+            handle.Rotate(0f, 0f, i);
+            yield return new WaitForSeconds(0.1f);
+        }
+        HandlePulled();
+        for (int i = 0; i < 15; i +=5){
+            handle.Rotate(0f, 0f, -i);
+            yield return new WaitForSeconds(0.1f);
+        }
+           
+    }
+
+    private void CheckResults(){
+        if(string.Equals(rows[0].stoppedSlot, rows[1].stoppedSlot) && string.Equals(rows[0].stoppedSlot, rows[2].stoppedSlot) &&string.Equals(rows[0].stoppedSlot, rows[3].stoppedSlot) ){
+            switch (rows[0].stoppedSlot){
+                case "Diamond":
+                    prizeValue = 200;
+                    break;
+                case "Lemon":
+                    prizeValue = 5000;
+                    break;
+                case "Cherry":
+                    prizeValue = 3000;
+                    break;
+                case "Seven":
+                    prizeValue = 1500;
+                    break;
+                case "Bar":
+                    prizeValue = 800;
+                    break;
+                case "Watermellon":
+                    prizeValue = 600;
+                    break;
+                case "Crown":  
+                    prizeValue = 400;
+                    break;
+            }
+        }
+        /*else if(string.Equals(rows[0].stoppedSlot, rows[1].stoppedSlot)){
+            switch (rows[0].stoppedSlot){
+                case "Diamond":
+                    prizeValue = 100;
+                    break;
+                case "Lemon":
+                    prizeValue = 4000;
+                    break;
+                case "Cherry":
+                    prizeValue = 2000;
+                    break;
+                case "Seven":
+                    prizeValue = 1000;
+                    break;
+                case "Bar":
+                    prizeValue = 700;
+                    break;
+                case "Watermellon":
+                    prizeValue = 500;
+                    break;
+                case "Crown":  
+                    prizeValue = 300;
+                    break;
+            }
+        }*/
+        prizeText.text = "Prize: " + prizeValue;
+        resultsChecked = true;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+}
---
+++ Assets/Scripts/Row.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Row : MonoBehaviour
+{
+    private int randomValue;
+    private float timeInterval;
+
+    public bool rowStopped;
+    public string stoppedSlot;
+
+    public IDictionary<double, string> slotValues = new Dictionary<double, string>()
+                                                    {
+                                                        {1.75, "Diamond"},
+                                                        {1, "Lemon"},
+                                                        {0.25, "Cherry"},
+                                                        {-0.5, "Seven"},
+                                                        {-1.25, "Bar"},
+                                                        {-2, "Watermellon"},
+                                                        {-2.75, "Crown"},
+                                                        {-3.5,"Diamond"}
+                                                    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        rowStopped = true;
+        GameControl.HandlePulled += StartRotating;
+    }
+
+
+    private void StartRotating(){
+        stoppedSlot = "";
+        StartCoroutine("Rotate");
+    }
+
+    private IEnumerator Rotate()
+    {
+        rowStopped = false;
+        timeInterval = 0.025f;
+        for (int i = 0; i < 30; i++){
+            if (transform.position.y <= -3.5f)
+                transform.position = new Vector2(transform.position.x, 1.75f);
+
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.75f);
+            yield return new WaitForSeconds(timeInterval);
+        }
+        randomValue = Random.Range(60, 100);
+
+        switch (randomValue % 3)
+        {
+            case 1:
+                randomValue += 2;
+                break;
+            case 2:
+                randomValue += 1;
+                break;
+        }
+        for (int i = 0; i < randomValue; i++){
+            if (transform.position.y <= -3.5)
+                transform.position = new Vector2(transform.position.x, 1.75f);
+
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.25f);
+
+            if (i > Mathf.RoundToInt(randomValue *0.25f))
+                timeInterval = 0.05f;
+            if (i > Mathf.RoundToInt(randomValue *0.5f))
+                timeInterval = 0.1f;
+            if (i > Mathf.RoundToInt(randomValue *0.75f))
+                timeInterval = 0.15f;
+            if (i > Mathf.RoundToInt(randomValue *0.95f))
+                timeInterval = 0.2f;
+
+            yield return new WaitForSeconds(timeInterval);
+        }
+            stoppedSlot =  slotValues[transform.position.y];
+            rowStopped = true;
+    }
+
+    private void slotValue(float y){
+
+    }
+
+    private void OnDestroy()
+    {
+        GameControl.HandlePulled -= StartRotating;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
