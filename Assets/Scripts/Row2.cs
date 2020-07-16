a9f18c0fd427fc636d8ab509edbe804dce6e1949
DESKTOP-V5HLD8G\Antfeeder <jesuskevinsilva@gmail.com>
2020-07-16 00:45:02 -0700

    Added "Green" scene to build

---
+++ Assets/Scripts/Row2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Row2 : MonoBehaviour
+{
+
+    [SerializeField]
+    public SpriteRenderer spriteRenderer;
+    public double stopTime;
+
+
+    [SerializeField]
+    private Sprite[] sprites = null;
+    private double timeInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        timeInterval = Time.realtimeSinceStartup;
+        if(timeInterval < stopTime)
+        spriteRenderer.sprite = sprites[Random.Range(0,6)];
+    }
+}
